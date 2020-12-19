cf93067aae2a136a186ac6eb7e5beff8232a0064
KomoreKalu <komorekalu@outlook.com>
2020-12-20 00:19:51 +0800

    Update update-binary.sh

--- update-binary.sh
+++ update-binary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 unzip $1 META-INF/com/google/android/update-binary -d .
-sh -c 'wget -c https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh -O - | cat - > ./META-INF/com/google/android/update-binary'
+wget -c https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh -O - | cat - > ./META-INF/com/google/android/update-binary
 zip $1 META-INF/com/google/android/update-binary
 rm -rf META-INF/
