8a3291271e3ef174c08377e14bde1c9cb95702e1
KomoreKalu <komorekalu@outlook.com>
2020-12-22 17:42:19 +0800

    Update update-binary.sh

--- update-binary.sh
+++ update-binary.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+IFS_TMP=$IFS
+IFS=$'\n'
 
-unzip $1 META-INF/com/google/android/update-binary -d .
-wget -c https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh -O - | cat - > ./META-INF/com/google/android/update-binary
-zip $1 META-INF/com/google/android/update-binary
-rm -rf META-INF/
+wget -c https://cdn.jsdelivr.net/gh/topjohnwu/Magisk/scripts/module_installer.sh
+
+if [ ${#} -eq 0 ]; then
+	for pkg in $(ls | grep '.*.zip$')
+	do
+		unzip -o $pkg META-INF/com/google/android/update-binary -d . 
+		cat module_installer.sh > ./META-INF/com/google/android/update-binary
+		zip $pkg META-INF/com/google/android/update-binary
+		echo $pkg update successfully
+	done
+else
+	for pkg in $@
+	do
+		unzip -o $pkg META-INF/com/google/android/update-binary -d .
+		cat module_installer.sh > ./META-INF/com/google/android/update-binary
+		zip $pkg META-INF/com/google/android/update-binary
+		echo $pkg update successfully
+	done
+fi	
+
+rm -rf META-INF module_installer.sh
+
+IFS=$IFS_TMP
